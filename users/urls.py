e893e9c82739f40ff991ae7b225e36e420a34acf
sivakrishna1993 <krishnarajarapu.scm@gmail.com>
2019-08-12 00:03:16 +0530

    my first commit

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,9 @@
-urlspatterns = [
+from cryptography.x509 import name
+from django.conf.urls import url
 
+from users import views
+
+urlpatterns = [
+    url(r'^siva/$',views.siva, name='siva'),
+    url(r'^time/$',views.today_is, name='time')
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from django.db.models.functions import datetime
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def siva(request):
+    return HttpResponse("<p> hello siva krishna welcome to django </p>")
+def today_is(request):
+    now = datetime.datetime.now()
+    html = "<p>Current date and time is :{0}</p>".format(now)
+    return HttpResponse(now)
